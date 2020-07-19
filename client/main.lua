a3a969713b26c2ff783aabc02dd368ab5b2d1f7e
Rubylium <alexisoko0@gmail.com>
2020-07-19 12:49:34 +0200

    Reworked system with permission
    Adding perm arg to config to create menu and options based on player permission level. This is an exemple usage of the system

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,17 @@
 local MenuOpen = false
 local playerInventory = {}
+local mileage = 0
+local perm = 0
 local selected = {
     item = nil,
     count = nil,
 }
+
+
+RegisterNetEvent("rFw:GetCarMileage")
+AddEventHandler("rFw:GetCarMileage", function(km)
+    mileage = km
+end)
 
 RegisterKeyMapping("open", "Open player menu", 'keyboard', config.key)
 RegisterCommand("open", function()
@@ -23,7 +31,7 @@ RMenu:Get('personnal', 'inventory').Closed = function()
     -- TODO Perform action
 end;
 
-RMenu.Add('personnal', 'inventory_use', RageUI.CreateSubMenu(RMenu:Get('personnal', 'main'), "Inventory", "~b~Player inventory"))
+RMenu.Add('personnal', 'inventory_use', RageUI.CreateSubMenu(RMenu:Get('personnal', 'inventory'), "Inventory", "~b~Player inventory"))
 RMenu:Get('personnal', 'inventory_use').EnableMouse = false
 RMenu:Get('personnal', 'inventory_use').Closed = function()
     -- TODO Perform action
@@ -46,17 +54,32 @@ function OpenPersonnalMenu()
     else
         MenuOpen = true
         RageUI.Visible(RMenu:Get('personnal', 'main'), true)
+        if config.carmileage then
+            TriggerServerEvent("rFw:GetCarMileage", GetVehicleNumberPlateText(GetVehiclePedIsIn(GetPlayerPed(-1), false)))
+        end
+        perm = exports.rFw:GetPlayerPermLevel()
         Citizen.CreateThread(function()
             while MenuOpen do
                 RageUI.IsVisible(RMenu:Get('personnal', 'main'), true, true, true, function()
+                    if config.carmileage then
+                        if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
+                            RageUI.ButtonWithStyle("Car mileage: ", nil, { RightLabel = "~b~"..mileage.." Km" }, true, function(_, _, s)
+                            end)
+                        end
+                    end
+
+                    RageUI.Button("Perm level: ~b~"..perm, nil, true, function(_, _, s)
+                    end)
+
                     RageUI.ButtonWithStyle("Inventaire", nil, { RightLabel = "→" }, true, function(_, _, s)
                         if s then
                             playerInventory = exports.rFw:GetPlayerInv()
                         end 
                     end, RMenu:Get('personnal', 'inventory'))
-            
+
+                    
                     for k,v in pairs(config.menu) do
-                        RageUI.ButtonWithStyle(v.name, nil, { RightLabel = "→" }, true, function(_, _, s)
+                        RageUI.ButtonWithStyle(v.name, nil, { RightLabel = "→" }, perm >= v.min_perm, function(_, _, s)
                         end, RMenu:Get('personnal', v.name))
                     end
                 end, function()
@@ -65,7 +88,7 @@ function OpenPersonnalMenu()
                 for k,v in pairs(config.menu) do
                     RageUI.IsVisible(RMenu:Get('personnal', v.name), true, true, true, function()
                         for _,i in pairs(v.options) do
-                            RageUI.Button(i.name, nil, true, function(_, _, s)
+                            RageUI.Button(i.name, nil, perm >= i.min_perm, function(_, _, s)
                                 if s then
                                     i.action()
                                 end
--- config.lua
+++ config.lua
@@ -1,24 +1,29 @@
 config = {
     key = "F5",
+    carmileage = true,
 
     menu = {
         {
             name = "Vehicle options",
+            min_perm = 0,
             options = {
                 {
                     name = "Open hood",
+                    min_perm = 0,
                     action = function()
                         SetVehicleDoorOpen(GetVehiclePedIsIn(GetPlayerPed(-1), false), 4, 0, true)
                     end,
                 },
                 {
                     name = "Close hood",
+                    min_perm = 0,
                     action = function()
                         SetVehicleDoorShut(GetVehiclePedIsIn(GetPlayerPed(-1), false), 4, true)
                     end,
                 },
                 {
                     name = "Open trunk",
+                    min_perm = 0,
                     action = function()
                         for i = 5,7 do
                             SetVehicleDoorOpen(GetVehiclePedIsIn(GetPlayerPed(-1), false), i, 0, true)
@@ -27,6 +32,7 @@ config = {
                 },
                 {
                     name = "Close trunk",
+                    min_perm = 0,
                     action = function()
                         for i = 5,7 do
                             SetVehicleDoorShut(GetVehiclePedIsIn(GetPlayerPed(-1), false), i, true)
@@ -37,9 +43,11 @@ config = {
         },
         {
             name = "Divers",
+            min_perm = 0,
             options = {
                 {
                     name = "Toogle Minimap",
+                    min_perm = 0,
                     action = function()
                         if radar == nil then radar = false end
                         DisplayRadar(radar)
@@ -48,5 +56,40 @@ config = {
                 },
             },
         },
+        {
+            name = "Staff options",
+            min_perm = 1,
+            options = {
+                {
+                    name = "Test",
+                    min_perm = 1,
+                    action = function()
+                        
+                    end,
+                },
+                {
+                    name = "Test",
+                    min_perm = 2,
+                    action = function()
+                        
+                    end,
+                },
+                {
+                    name = "Test",
+                    min_perm = 3,
+                    action = function()
+                        
+                    end,
+                },
+                {
+                    name = "Test",
+                    min_perm = 4,
+                    action = function()
+                        
+                    end,
+                },
+            },
+        },
     },
+
 }
